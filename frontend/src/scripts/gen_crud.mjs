83d511276ae3d4f9987f51143a496317375de804
lyongaming <owlsmouth@gmail.com>
2023-07-26 17:15:52 -0400

    Crud Generator checks if table exists

--- frontend/src/scripts/gen_crud.mjs
+++ frontend/src/scripts/gen_crud.mjs
@@ -7,22 +7,35 @@ const __dirname = path.dirname(__filename);
 let filePath = path.join(__dirname, "crud_template.txt");
 let fileContent = fs.readFileSync(filePath, "utf-8");
 
-fileContent = fileContent.replace(/tablename/g, process.argv[2]);
+let tablenames = [];
 
-const pagesPath = path.join(__dirname, "../pages");
-const dirPath = path.join(__dirname, `../pages/${[process.argv[2]]}`);
-filePath = path.join(__dirname, `../pages/${[process.argv[2]]}/${[process.argv[2]]}.jsx`);
-if(fs.existsSync(pagesPath)) {
-    if(fs.existsSync(filePath)) {
-        console.log("The file already exists")
+(async() => {
+    const response = await fetch("http://localhost:8080/tables");
+    const data = await response.json();
+    tablenames = data.tables.map(table => table.name);
+
+    if(tablenames.find(tablename => tablename === `${process.argv[2]}`)) {
+        fileContent = fileContent.replace(/tablename/g, process.argv[2]);
+        
+        const pagesPath = path.join(__dirname, "../pages");
+        const dirPath = path.join(__dirname, `../pages/${[process.argv[2]]}`);
+        filePath = path.join(__dirname, `../pages/${[process.argv[2]]}/${[process.argv[2]]}.jsx`);
+        if(fs.existsSync(pagesPath)) {
+            if(fs.existsSync(filePath)) {
+                console.log("The file already exists")
+            } else {
+                fs.mkdirSync(dirPath);
+                fs.writeFileSync(filePath, fileContent);
+                console.log(`${process.argv[2]}.jsx file created`);
+            }
+        } else {
+            fs.mkdirSync(pagesPath);
+            fs.mkdirSync(dirPath);
+            fs.writeFileSync(filePath, fileContent);
+            console.log(`${process.argv[2]}.jsx file created`);
+        }
     } else {
-        fs.mkdirSync(dirPath);
-        fs.writeFileSync(filePath, fileContent);
-        console.log(`${process.argv[2]}.jsx file created`);
+        console.log(`${process.argv[2]} table doesn't exist`);
     }
-} else {
-    fs.mkdirSync(pagesPath);
-    fs.mkdirSync(dirPath);
-    fs.writeFileSync(filePath, fileContent);
-    console.log(`${process.argv[2]}.jsx file created`);
-}
+
+})();
